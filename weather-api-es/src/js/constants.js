36ac85e51047b00edc7030523f49bdcc23749a78
RahatAli <rahatali@A006-00935.local>
2024-02-16 10:20:38 +0500

    Security fixes

--- weather-api-es/src/js/constants.js
+++ weather-api-es/src/js/constants.js
@@ -1,5 +1,5 @@
-// Left Empty
+// dba6ed565e0d72778b7b4c1afaca1857
 export default {
     weatherUrl: 'https://api.openweathermap.org/data/2.5/weather',
-    appid: 'dba6ed565e0d72778b7b4c1afaca1857'
+    appid: ''
 }
